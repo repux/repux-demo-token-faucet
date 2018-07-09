3f5878efbf262b96930ff65bb6a8d715bef968d7
Przemyslaw Kocznur <pkocznur@gmail.com>
2018-07-09 17:42:58 +0200

    Fix - webpack didnt read env variables

--- config/config.js
+++ config/config.js
@@ -5,10 +5,11 @@ const config = {
         host: process.env.FAUCET_SERVER_HOST || 'localhost'
     },
     ssl: {
-        keyPath: process.env.FAUCET_SSL_KEY_PATH || '/shared/ssl/cert.key',
-        certPath: process.env.FAUCET_SSL_CERT_PATH || '/shared/ssl/cert.pem'
+        keyPath: process.env.FAUCET_SSL_KEY_PATH || './ssl/cert.key',
+        certPath: process.env.FAUCET_SSL_CERT_PATH || './ssl/cert.pem'
     },
-    demoTokenAddress: process.env.FAUCET_DEMO_TOKEN_ADDRESS || '0xfa19d4e302336d61b895ea3b26bf4864bdd1d8ab',
+
+    demoTokenAddress: process.env.FAUCET_DEMO_TOKEN_ADDRESS || '0x618231c15b548292abc0013da5e24bab350c86d2',
     account: process.env.FAUCET_ACCOUNT || '0x07f486c4ec2ade092abfe3261d0fc891e737e689',
     giveawayTokenAmounts: process.env.FAUCET_GIVEAWAY_TOKENS_AMOUNT || 100, // in ether
     ethereumHost: process.env.FAUCET_ETHEREUM_HOST || 'http://127.0.0.1:8545',
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/frontend/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new Dotenv({
+      path: './config/.env', 
+      safe: false,
+      systemvars: true,
+      silent: false
+    })
+  ]
 };
